db72e1f3dfd218f2e17c92f5cc4a260eca4757af
Warsono <warsono16694@gmail.com>
2022-04-04 14:42:38 +0700

    demo: add getter

--- demo/src/views/CounterView.vue
+++ demo/src/views/CounterView.vue
@@ -8,6 +8,8 @@ const counter = useCounterStore();
   <main>
     <h1>Count: {{ counter.counter }}</h1>
 
+    <h3>Getter: {{ counter.doubleCount }}</h3>
+
     <button type="button" @click="counter.decrement">Decrement</button>
     <button type="button" @click="counter.increment">Increment</button>
   </main>
